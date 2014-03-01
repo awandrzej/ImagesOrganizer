0d3bdedf3519e265f2f6ceeb99b9dba42d95f700
andrzej <andrzej.wudara@gmail.com>
2014-03-01 15:59:39 +0100

    elo

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
-
+import com.andrzej.imageTools.ArrayToFile;
 
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	List <String> listImg = new ArrayList<String>();
 	
-		Welcomer welcomer = new Welcomer();
-		welcomer.sayHello();
+	public static void main(String[] args) {
 		
+		HelloWorld t = new HelloWorld();
+		t.addElements();
+	}
+	
+	private void addElements  (){
+		for (int i=0;i<10;i++){
+			listImg.add("name"); 
+			}
+		System.out.println(this.listImg.get(5));
+		
+
 	}
 	
 }
